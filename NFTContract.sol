edaadc73458221f484c4acfa90c30ea5ad6631a6
Aaquib Dilkash <aaquibdilkash@gmail.com>
2022-05-10 16:56:20 +0530

    Create NFTContract.sol

---
+++ NFTContract.sol
@@ -0,0 +1,59 @@
+pragma solidity >=0.8.0;
+
+contract NFT {
+    // constructor() {
+
+    // }
+    uint256 public id;
+
+    struct NFT {
+        uint256 id;
+        string name;
+        uint256 cost;
+        address owner;
+    }
+
+    mapping(uint=> NFT) public nftMapping;
+
+    function mint(string memory _name, uint256 _cost) public {
+        nftMapping[id] = NFT(
+            id,
+            _name,
+            _cost,
+            msg.sender
+        );
+
+        id++;
+
+    }
+
+    function deleteNFT(uint256 _id) public {
+
+        address _owner = nftMapping[_id].owner;
+        require(_owner != address(0), "This NFT doesn't exist!");
+        require(_owner == msg.sender, "Only Owner can delete the NFT!");
+
+        nftMapping[_id] = NFT(
+            0,
+            "",
+            0,
+            address(0)
+        );
+
+    }
+
+    function updateNFT(uint256 _id, string memory _name, uint256 _cost) public {
+        address _owner = nftMapping[_id].owner;
+        require(_owner != address(0), "This NFT doesn't exist!");
+        require(_owner == msg.sender, "Only Owner can delete the NFT!");
+
+        nftMapping[_id] = NFT(
+            _id,
+            _name,
+            _cost,
+            msg.sender
+        );
+    }
+
+
+}
